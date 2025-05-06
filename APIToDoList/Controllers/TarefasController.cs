a444e05570f709aee49862c4518dde050705675e
João Pedro Medeiros Santos <peterjp97@hotmail.com>
2025-05-06 19:38:02 -0300

    Add project files.

---
+++ APIToDoList/Controllers/TarefasController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIToDoList.Controllers;
+
+[ApiController]
+[Route("api/[controller]")
+public class TarefasController : ControllerBase
+{
+    
+}
---
+++ APIToDoList/Models/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace APIToDoList.Models;
+
+public class Tarefa
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Descricao { get; set; }
+    public bool Status { get; set; }
+    public DateTime DataCriacao { get; set; }
+    public DateTime? DataConclusao { get; set; }
+}
