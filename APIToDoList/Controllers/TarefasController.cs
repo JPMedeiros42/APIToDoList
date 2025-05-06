6a4c7227d19d8a5b6de73f88ecd6ed9e5a6877ea
João Pedro Medeiros Santos <peterjp97@hotmail.com>
2025-05-06 20:22:23 -0300

    Criação da API básica de ToDoList (Desafio 1)

--- APIToDoList/Controllers/TarefasController.cs
+++ APIToDoList/Controllers/TarefasController.cs
@@ -1,10 +1,97 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using APIToDoList.Models;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace APIToDoList.Controllers;
 
 [ApiController]
-[Route("api/[controller]")
+[Route("api/[controller]")]
 public class TarefasController : ControllerBase
 {
-    
+    private static List<Tarefa> tarefas = new List<Tarefa>();
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(tarefas);
+    }
+
+    [HttpGet("{id:int}", Name = "ObterTarefa")]
+    public IActionResult GetById(int id)
+    {
+        var tarefa = tarefas.FirstOrDefault(t => t.Id == id);
+        if (tarefa == null)
+        {
+            return NotFound("Tarefa inválida");
+        }
+        return Ok(tarefa);
+    }
+
+    [HttpPost]
+    public IActionResult Post(Tarefa tarefa)
+    {
+        if (tarefa is null)
+        {
+            return BadRequest("Tarefa inválida");
+        }
+
+        tarefa.Id = tarefas.Count > 0 ? tarefas.Max(t => t.Id) + 1 : 1; 
+        tarefa.DataCriacao = DateTime.Now;
+        tarefa.Status = false;
+        tarefas.Add(tarefa);
+
+        return new CreatedAtRouteResult("ObterTarefa",
+            new {id = tarefa.Id}, tarefa);
+    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult Put(int id, Tarefa tarefa)
+    {
+        if (tarefa is null || tarefa.Id != id)
+        {
+            return BadRequest("Tarefa inválida");
+        }
+
+        var tarefaExistente = tarefas.FirstOrDefault(t => t.Id == id);
+        if (tarefaExistente == null)
+        {
+            return NotFound("Tarefa inválida");
+        }
+
+        tarefaExistente.Titulo = tarefa.Titulo;
+        tarefaExistente.Descricao = tarefa.Descricao;
+        tarefaExistente.Status = tarefa.Status;
+
+        if (tarefa.Status)
+        {
+            tarefaExistente.DataConclusao = DateTime.Now;
+        }
+        else
+        {
+            tarefaExistente.DataConclusao = null;
+        }
+
+        return Ok(new
+        {
+            mensagem = "Tarefa atualizada com sucesso",
+            dados = tarefaExistente
+        });
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult Delete(int id)
+    {
+        var tarefa = tarefas.FirstOrDefault(t => t.Id == id);
+        if (tarefa == null)
+        {
+            return NotFound("Tarefa inválida");
+        }
+
+        tarefas.Remove(tarefa);
+        return Ok(new
+        {
+            mensagem = "Tarefa excluida com sucesso",
+            dados = tarefa
+        });
+    }
 }
--- APIToDoList/Models/Tarefa.cs
+++ APIToDoList/Models/Tarefa.cs
@@ -1,11 +1,24 @@
-﻿namespace APIToDoList.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace APIToDoList.Models;
 
 public class Tarefa
 {
+    [Key]
     public int Id { get; set; }
-    public string Titulo { get; set; }
-    public string Descricao { get; set; }
-    public bool Status { get; set; }
-    public DateTime DataCriacao { get; set; }
+
+    [Required(ErrorMessage = "O título é obrigatório.")]
+    [StringLength(60, MinimumLength = 5, ErrorMessage ="O Título deve conter entre 5 e 60 caracteres")]
+    public string? Titulo { get; set; }
+
+    [Required(ErrorMessage = "A descrição é obrigatória.")]
+    [StringLength(100, MinimumLength = 10, ErrorMessage = "A descrição deve conter entre 10 e 100 caracteres")]
+    public string? Descricao { get; set; }
+
+    [Required(ErrorMessage = "A data de conclusão é obrigatória.")]
+    public bool Status { get; set; } = false;
+
+    public DateTime DataCriacao { get; set; } = DateTime.Now;
     public DateTime? DataConclusao { get; set; }
 }
